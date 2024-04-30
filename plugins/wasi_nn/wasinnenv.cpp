369c2dfb8cacb54aaf9c766e9d97638081349108
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-04-30 18:14:10 +0800

    [misc] fix plugin
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_nn/wasinnenv.cpp
+++ plugins/wasi_nn/wasinnenv.cpp
@@ -158,6 +158,8 @@ Plugin::Plugin::PluginDescriptor Descriptor{
     /* Version */ {0, 10, 1, 0},
     /* ModuleCount */ 1,
     /* ModuleDescriptions */ MD,
+    /* ComponentCount */ 0,
+    /* ComponentDescriptions */ nullptr,
     /* AddOptions */ addOptions,
 };
 } // namespace
